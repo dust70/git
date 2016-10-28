8ee4ef3af13dc92f2bb6495cc87e79f5ade5a8be
René Six <info@renatius.de>
2016-10-29 00:08:54 +0200

    add file template/hooks/php/check.sh to repository

---
+++ template/hooks/php/check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source "${GIT_DIR}"/hooks/config.sh
+source "${HOOKS_DIR}"/base/change_detector.sh
+
+build_base()
+{
+    "${HOOKS_DIR}"/base/ctags "${1}"
+    "${HOOKS_DIR}"/base/cscope "${1}"
+}
+
+build_php()
+{
+    COMPOSER_JSON="${PROJECT_DIR}"/composer.json
+    if [ -e "${COMPOSER_JSON}" ] && has_changed ${1} "${COMPOSER_JSON}"; then
+        "${HOOKS_DIR}"/php/composer.sh
+    fi
+
+    build_base php
+}
+
+if which php > /dev/null 2>&1; then
+    build_php
+fi
