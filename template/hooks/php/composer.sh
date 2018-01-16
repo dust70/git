723a1fa69078a92b0b691d38871eeff27b11bcaa
René Six <info@renatius.de>
2018-01-16 12:25:22 +0100

    Fix issue with systemwide installed composer

--- template/hooks/php/composer.sh
+++ template/hooks/php/composer.sh
@@ -3,21 +3,26 @@
 GIT_DIR="${GIT_DIR:-${PWD}/.git}"
 source "${GIT_DIR}"/hooks/config.sh
 
-COMPOSER_DIR="${PROJECT_DIR}"/bin
-COMPOSER="${COMPOSER_DIR}"/composer
+if hash composer > /dev/null 2>&1; then
+    COMPOSER="composer"
+else
+    COMPOSER="${PROJECT_DIR}"/bin/composer
 
-if [ ! -x "${COMPOSER}" ]; then
-    test ! -d "${COMPOSER_DIR}" && mkdir "${COMPOSER_DIR}"
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    if [ ! -e "${COMPOSER}" ]; then
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 
-    EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
-    ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+        EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+        ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-    if [ "${EXPECTED_SIGNATURE}" = "${ACTUAL_SIGNATURE}" ]; then
-        php ./composer-setup.php --install-dir="${COMPOSER_DIR}" --filename=composer --quiet || exit $?
+        if [ "${EXPECTED_SIGNATURE}" = "${ACTUAL_SIGNATURE}" ]; then
+            mkdir bin >> /dev/null 2>&1
+            php ./composer-setup.php --install-dir="bin" --filename=composer
+        fi
+
+        rm -f ./composer-setup.php
     fi
 
-    rm -f ./composer-setup.php
+    COMPOSER="php ${COMPOSER}"
 fi
 
-php "${COMPOSER}" install --no-interaction --optimize-autoloader --prefer-source
+"${COMPOSER}" install --no-interaction --optimize-autoloader --prefer-source
