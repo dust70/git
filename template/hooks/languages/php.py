32362ff86d7b2433e32ce1db4e4aa66d2541de72
René Six <info@renatius.de>
2018-12-15 18:33:28 +0100

    Use abstract class in php linter

--- template/hooks/languages/php.py
+++ template/hooks/languages/php.py
@@ -1,6 +1,5 @@
+from languages.linty import Linty
 from languages.speakable import Speakable
-
-import subprocess
 
 
 class Composer(Speakable):
@@ -16,15 +15,12 @@ class Composer(Speakable):
         return self.command + ["install", "--no-interaction"]
 
 
-class Php:
-    def precommit(self):
-        process = subprocess.Popen(
-            ["git", "diff-index", "--diff-filter=ACMRT", "--name-only", "HEAD", "--"],
-            stdout=subprocess.PIPE
-        )
-        for l in process.stdout.readlines():
-            check = subprocess.Popen(["php", "-l", l.strip(), ">", "/dev/null"])
-            check.communicate()
+class Php(Linty):
+    def get_command(self, file):
+        return ["php", "-l", file]
 
-            if check.returncode != 0:
-                raise Exception("Command don't succeed!")
+    def get_config(self):
+        return {}
+
+    def get_key(self):
+        return "php"
